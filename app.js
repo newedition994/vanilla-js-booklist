d1933cc212408b3179b2e8d40a6f6830843d4df2
Breyanna Vass <breyannavass@gmail.com>
2019-12-19 20:46:53 -0500

    get books and add books to the store class

--- app.js
+++ app.js
@@ -51,3 +51,23 @@ class UI {
     document.querySelector("#isbn").value = "";
   }
 }
+
+// Store Class - handle storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}

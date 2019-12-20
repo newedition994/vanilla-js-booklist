9468e25a6d9d60866556c860192c0a8c18c7fa38
Breyanna Vass <breyannavass@gmail.com>
2019-12-19 20:34:56 -0500

    made a static addtolist and deletebook function

--- app.js
+++ app.js
@@ -13,4 +13,23 @@ class UI {
     const books = Store.getBooks();
     books.forEach(book => UI.addBookToList(book));
   }
+
+  static addBookToList(book) {
+    const list = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+    list.appendChild(row);
+  }
+
+  static deleteBook(el) {
+    if (el.classList.contains("delete")) {
+      el.parentElement.parentElement.remove();
+    }
+  }
 }

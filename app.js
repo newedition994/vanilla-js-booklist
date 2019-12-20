1cfcbf90fec09c5544e9e2d3416897e5cc03cebd
Breyanna Vass <breyannavass@gmail.com>
2019-12-19 20:54:53 -0500

    validate the content

--- app.js
+++ app.js
@@ -95,4 +95,24 @@ document.querySelector("#book-form").addEventListener("submit", e => {
   const title = document.querySelector("#title").value;
   const author = document.querySelector("#author").value;
   const isbn = document.querySelector("#isbn").value;
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert("Book Added", "success");
+
+    // Clear fields
+    UI.clearFields();
+  }
 });
